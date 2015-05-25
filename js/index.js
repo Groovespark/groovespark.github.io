b03524bcc8a6121d1e72de814e02de387cd1c6a0
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-05-25 23:45:49 +0200

    Making things work

--- js/index.js
+++ js/index.js
@@ -3,18 +3,38 @@ jsonpproxy = 'http://jsonpify.herokuapp.com?resource=';
 $(function() {
   // get playlist
   $("#gs-playsearch").click(function(e) {
-    gsplaylist = 'https://raw.githubusercontent.com/'+$('.gs-username').val()+'/gs-playlist/master/';
+    gsplaylist = 'https://raw.githubusercontent.com/'+encodeURI($('.gs-username').val())+'/gs-playlists/master/';
     gsplaylistindex = gsplaylist + 'index.json';
 
     $.ajax({                                                                                      
       url: jsonpproxy + gsplaylistindex,  
       dataType: 'jsonp',                                                                          
       success: function(data){
-        $('.gs-playlist').html('');
-        json = JSON.parse(data);  
+        var playlists = $('.gs-playlists');
+        playlists.html('');
+        
+        var playlistjson;
+        json = JSON.parse(data);
+        
         $.each(json['playlists'], function(index, val) {
-          $('.gs-playlist').append('<li>'+ json['playlists'][index]['pathname'].replace('.json', '') +'</li>')
-          console.log(json['playlists'][index]['pathname']);
+          jsonplaylists = json['playlists'][index];
+        
+          
+          playlists.append('<li>'
+            + '<a href="#'+ jsonplaylists['pathname'].replace('.json', '') +'">'+ jsonplaylists['pathname'].replace('.json', '') + '</a>'
+            + '<ul class="gs-playlistall">');
+          $.ajax({                                                                                      
+            url: jsonpproxy + gsplaylist + jsonplaylists['pathname'],  
+            dataType: 'jsonp',                                                                          
+            success: function(dataPlay){
+              playlistjson = JSON.parse(dataPlay);
+              $.each(playlistjson['songs'], function(index, val) {
+                song = playlistjson['songs'][index];
+                $('<li><a class="gs-playsong" href="#'+ song['id']+'">' + song['track'] + ' - '+ song['artist'] +'</a></li>').appendTo('.gs-playlistall', this);
+              });
+            }
+          });
+          playlists.append('</ul></li>');
         });
       }
     });
@@ -33,11 +53,29 @@ $(function() {
         console.log(data);
         $.each(data['tracks'], function(index, val) {
           song = data['tracks'][index];
-          $('.gs-songs').append('<li><a href="'+ song['file']+'">' + song['track'] + ' - '+ song['artist'] +'</a></li>')
+          $('.gs-songs').append('<li><a class="gs-playsong" href="#'+ song['id']+'">' + song['track'] + ' - '+ song['artist'] +'</a></li>');
         });
       }
     });
     e.preventDefault();
+  });
+
+  // Music player
+  $("body").on('click', '.gs-playsong', function(event) {
+    var hash = window.location.hash.substring(1);
+    var player = document.getElementById("audio");
+    player.src = "http://pleer.com/browser-extension/files/" + hash+ ".mp3";
+    player.play();
+  });
+
+  // Playlist expand
+  $("body").on('click', '.gs-playlist', function(event) {
+    var pl = $(this+".gs-playlistall");
+    if (pl.hasClass('ex-playlist')) {
+      pl.removeClass('ex-playlist');
+    } else {
+      pl.addClass('ex-playlist');
+    }
   });
 
 });
