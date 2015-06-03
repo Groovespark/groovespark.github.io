2752686e78ee6f1b46357eb1d2d6bc2f860acccb
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-06-03 13:54:49 +0200

    trying routes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,24 @@ githubproxy = 'http://github-raw-cors-proxy.herokuapp.com/';
 
 
 $(function() {
+  var author = function () { console.log("author"); };
+  var books = function () { console.log("books"); };
+  var viewBook = function (bookId) {
+    console.log("viewBook: bookId is populated: " + bookId);
+  };
+
+  var routes = {
+    '/author': author,
+    '/books': [books, function() {
+      console.log("An inline route handler.");
+    }],
+    '/books/view/:bookId': viewBook
+  };
+
+  var router = Router(routes);
+
+  router.init();
+
   var player = document.createElement('audio');
   var playercover = $('.img-cover');
   var playlists = $('.gs-playlists');
